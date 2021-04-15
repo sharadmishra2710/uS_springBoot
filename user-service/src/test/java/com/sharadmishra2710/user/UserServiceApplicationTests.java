610c0cb7fbb66395828e2dbd5fb6d3358dc3e981
sharad <sharad13102185@gmail.com>
2021-04-15 21:02:22 +0530

    added user service

---
+++ user-service/src/test/java/com/sharadmishra2710/user/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharadmishra2710.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
